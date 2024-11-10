9e5f12e047c039ad6a10f4f442d086c617837d94
Kerem Kulaç <keremkulac14@gmail.com>
2024-11-10 15:08:07 +0300

    AddFuelPurchaseFragment designed

---
+++ app/src/main/java/com/keremkulac/journeylog/presentation/ui/addFuelPurchase/AddFuelPurchaseFragment.kt
@@ -0,0 +1,21 @@
+package com.keremkulac.journeylog.presentation.ui.addFuelPurchase
+
+import android.os.Bundle
+import android.view.View
+import com.keremkulac.journeylog.databinding.FragmentAddFuelPurchaseBinding
+import com.keremkulac.journeylog.util.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AddFuelPurchaseFragment :
+    BaseFragment<FragmentAddFuelPurchaseBinding>(FragmentAddFuelPurchaseBinding::inflate) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+
+
+}
--- app/src/main/java/com/keremkulac/journeylog/presentation/ui/fuelPurchase/FuelPurchaseFragment.kt
+++ app/src/main/java/com/keremkulac/journeylog/presentation/ui/fuelPurchase/FuelPurchaseFragment.kt
@@ -1,6 +1,10 @@
 package com.keremkulac.journeylog.presentation.ui.fuelPurchase
 
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.keremkulac.journeylog.R
 import com.keremkulac.journeylog.util.BaseFragment
 import com.keremkulac.journeylog.databinding.FragmentFuelPurchaseBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -8,4 +12,14 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class FuelPurchaseFragment : BaseFragment<FragmentFuelPurchaseBinding>(FragmentFuelPurchaseBinding::inflate) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navigateAddFuelPurchase()
+    }
+
+    private fun navigateAddFuelPurchase(){
+        binding.fab.setOnClickListener{
+            findNavController().navigate(R.id.action_fuelPurchaseFragment_to_addFuelPurchaseFragment)
+        }
+    }
 }
