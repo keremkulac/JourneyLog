d005b478de534bf1ac0db6e43946a66095472dde
Kerem Kulaç <keremkulac14@gmail.com>
2024-12-03 14:35:19 +0300

    Select vehicle fragment created

---
+++ app/src/main/java/com/keremkulac/journeylog/domain/model/Vehicle.kt
@@ -0,0 +1,6 @@
+package com.keremkulac.journeylog.domain.model
+
+data class Vehicle(
+    val iconResId: Int? = 0,
+    val title: String? = ""
+)
---
+++ app/src/main/java/com/keremkulac/journeylog/presentation/ui/vehicleSelect/VehicleSelectFragment.kt
@@ -0,0 +1,43 @@
+package com.keremkulac.journeylog.presentation.ui.vehicleSelect
+
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.keremkulac.journeylog.R
+import com.keremkulac.journeylog.databinding.FragmentVehicleSelectBinding
+import com.keremkulac.journeylog.domain.model.Vehicle
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.ArrayList
+
+@AndroidEntryPoint
+class VehicleSelectFragment : BottomSheetDialogFragment(R.layout.fragment_vehicle_select) {
+
+    private lateinit var binding: FragmentVehicleSelectBinding
+    private lateinit var adapter : VehicleAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentVehicleSelectBinding.bind(view)
+        createRecyclerView()
+        vehicleClick()
+    }
+
+    private fun createRecyclerView() {
+        adapter = VehicleAdapter()
+        val vehicleItems = listOf(
+            Vehicle(R.drawable.ic_bike, "Motorsiklet"),
+            Vehicle(R.drawable.ic_car, "Otomobil"),
+            Vehicle(R.drawable.ic_suv, "SUV"),
+            Vehicle(R.drawable.ic_van, "Ticari"),
+            Vehicle(R.drawable.ic_truck, "Kamyonet")
+        )
+        binding.selectVehicleRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        adapter.vehicleList = ArrayList(vehicleItems)
+        binding.selectVehicleRecyclerView.adapter = adapter
+    }
+
+    private fun vehicleClick(){
+        adapter.clickListener = {vehicle-> }
+    }
+}
