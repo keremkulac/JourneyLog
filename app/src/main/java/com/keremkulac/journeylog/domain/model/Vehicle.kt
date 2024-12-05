9d85ddd4ebe8cacebafd7b598459d95428b913a0
Kerem Kulaç <keremkulac14@gmail.com>
2024-12-05 17:49:20 +0300

    License plate fragment created

--- app/src/main/java/com/keremkulac/journeylog/domain/model/Vehicle.kt
+++ app/src/main/java/com/keremkulac/journeylog/domain/model/Vehicle.kt
@@ -1,6 +1,10 @@
 package com.keremkulac.journeylog.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Vehicle(
     val iconResId: Int? = 0,
     val title: String? = ""
-)
+) : Parcelable
