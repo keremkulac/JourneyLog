a6c71d8451ee4f35cdec2024b2b9cf6e16d11a8b
Kerem Kulaç <keremkulac14@gmail.com>
2024-12-06 19:26:01 +0300

    GetAllVehicles adapter created

---
+++ app/src/main/java/com/keremkulac/journeylog/presentation/ui/vehicleView/VehicleViewAdapter.kt
@@ -0,0 +1,63 @@
+package com.keremkulac.journeylog.presentation.ui.vehicleView
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.keremkulac.journeylog.R
+import com.keremkulac.journeylog.domain.model.Vehicle
+
+class VehicleViewAdapter : RecyclerView.Adapter<VehicleViewAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): VehicleViewAdapter.ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_all_vehicle, parent, false)
+        )
+    }
+
+    private val diffUtil = object : DiffUtil.ItemCallback<Vehicle>() {
+        override fun areItemsTheSame(
+            oldItem: Vehicle,
+            newItem: Vehicle
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(
+            oldItem: Vehicle,
+            newItem: Vehicle
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val recyclerListDiffer = AsyncListDiffer(this, diffUtil)
+    var vehicleList: ArrayList<Vehicle>
+        get() = recyclerListDiffer.currentList.toMutableList() as ArrayList<Vehicle>
+        set(value) {
+            recyclerListDiffer.submitList(value as List<Vehicle?>?)
+        }
+
+    override fun onBindViewHolder(holder: VehicleViewAdapter.ViewHolder, position: Int) {
+        holder.binds(vehicleList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return vehicleList.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun binds(vehicle: Vehicle) {
+            val icon: ImageView = itemView.findViewById(R.id.vehicleIcon)
+            val title: TextView = itemView.findViewById(R.id.vehicleTitle)
+            icon.setImageResource(vehicle.iconResId!!)
+            title.text = vehicle.title
+        }
+    }
+}
--- app/src/main/java/com/keremkulac/journeylog/presentation/ui/vehicleView/VehicleViewFragment.kt
+++ app/src/main/java/com/keremkulac/journeylog/presentation/ui/vehicleView/VehicleViewFragment.kt
@@ -2,14 +2,55 @@ package com.keremkulac.journeylog.presentation.ui.vehicleView
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.keremkulac.journeylog.databinding.FragmentVehicleViewBinding
+import com.keremkulac.journeylog.domain.model.Vehicle
 import com.keremkulac.journeylog.util.BaseFragment
+import com.keremkulac.journeylog.util.HandleResult
+import com.keremkulac.journeylog.util.SharedViewModel
+import java.util.ArrayList
 
 
-class VehicleViewFragment : BaseFragment<FragmentVehicleViewBinding>(FragmentVehicleViewBinding::inflate) {
+class VehicleViewFragment :
+    BaseFragment<FragmentVehicleViewBinding>(FragmentVehicleViewBinding::inflate) {
 
+    private lateinit var sharedViewModel: SharedViewModel
+    private val viewModel by viewModels<VehicleViewViewModel>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        sharedViewModel = ViewModelProvider(requireActivity())[SharedViewModel::class.java]
+        observeUser()
+        observeAllVehicles()
     }
+
+    private fun observeAllVehicles() {
+        viewModel.getAllVehicles.observe(viewLifecycleOwner) { result ->
+            HandleResult.handleResult(binding.progressBar, result,
+                onSuccess = { data ->
+                    val vehicleList = data as List<Vehicle>
+                    setRecyclerView(vehicleList)
+                }, onFailure = { message ->
+                    Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+                })
+        }
+    }
+
+    private fun observeUser() {
+        sharedViewModel.sharedData.observe(viewLifecycleOwner) { user ->
+            viewModel.getAllVehicles(user.id)
+        }
+    }
+
+    private fun setRecyclerView(vehicleList: List<Vehicle>) {
+        val adapter = VehicleViewAdapter()
+        binding.vehiclesRecyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        adapter.vehicleList = ArrayList(vehicleList)
+        binding.vehiclesRecyclerView.adapter = adapter
+    }
+
+
 }
