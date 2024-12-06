2f64a84575321a9b61e17dc3976c64709950f0b5
Kerem Kulaç <keremkulac14@gmail.com>
2024-12-06 18:56:57 +0300

    Vehicle view screen created

---
+++ app/src/main/java/com/keremkulac/journeylog/presentation/ui/vehicleView/VehicleViewFragment.kt
@@ -0,0 +1,15 @@
+package com.keremkulac.journeylog.presentation.ui.vehicleView
+
+import android.os.Bundle
+import android.view.View
+import com.keremkulac.journeylog.databinding.FragmentVehicleViewBinding
+import com.keremkulac.journeylog.util.BaseFragment
+
+
+class VehicleViewFragment : BaseFragment<FragmentVehicleViewBinding>(FragmentVehicleViewBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/keremkulac/journeylog/presentation/ui/vehicleView/VehicleViewViewModel.kt
@@ -0,0 +1,12 @@
+package com.keremkulac.journeylog.presentation.ui.vehicleView
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class VehicleViewViewModel @Inject constructor(
+
+): ViewModel() {
+
+}
