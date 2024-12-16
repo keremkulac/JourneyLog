d139763ed3b2878273ba3ea657136e64fa6fb368
Kerem Kulaç <keremkulac14@gmail.com>
2024-12-16 23:23:21 +0300

    AppSettingsFragment created

---
+++ app/src/main/java/com/keremkulac/journeylog/presentation/ui/appSettings/AppSettingsFragment.kt
@@ -0,0 +1,17 @@
+package com.keremkulac.journeylog.presentation.ui.appSettings
+
+import android.os.Bundle
+import android.view.View
+import com.keremkulac.journeylog.databinding.FragmentAppSettingsBinding
+import com.keremkulac.journeylog.util.BaseFragment
+
+
+class AppSettingsFragment :
+    BaseFragment<FragmentAppSettingsBinding>(FragmentAppSettingsBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
